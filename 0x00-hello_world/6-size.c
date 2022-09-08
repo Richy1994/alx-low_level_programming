9f243c5d96def6accf9748c3e311cbe6e6f5e3c4
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-08 16:50:37 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,11 @@
 
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-long mlong;
-long long mlonglong;
-int intType;
-float floatType;
-char charType;
-
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of long: %zu bytes\n", sizeof(mlong));
