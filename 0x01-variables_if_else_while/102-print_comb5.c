14284aea36c97efd1d0897225dc404f019e1dfba
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-11 16:13:14 -0700

    hello

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,7 @@
 * Description: Print all possible combinations of two two-digit numbers.
 * Numbers should range from 0 to 99.
 * The two numbers should be separated by a space.
-All numbers should be printed with two digits. 1 should be printed as 01.
+* All numbers should be printed with two digits. 1 should be printed as 01.
 * Combination of numbers must be separated by a comma followed by a space.
 * Combinations of numbers should be printed in ascending order.
 * `00 01` and `01 00` are considered as the same combination.
@@ -22,6 +22,7 @@ for (i = 0; i < 100; i++)
 {
 a = i / 10; /* doubles fnum */
 b = i % 10; /* singles fnum */
+
 
 for (j = 0; j < 100; j++)
 {
