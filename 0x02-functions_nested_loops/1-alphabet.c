691adb0a1cffa5231a18e0cd37ed8b872caa886b
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-13 16:09:57 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ *
+ */
+void print_alphabet(void)
+{
+char alph = 'a';
+for (alph = 'a'; alph < 'z'; alph++)
+{
+_putchar(alph);
+}
+_putchar('\n');
+
+}
