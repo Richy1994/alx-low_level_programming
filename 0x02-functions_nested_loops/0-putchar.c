667c332b04c33721490540053b50736b359e82c5
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-13 15:00:03 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
-* _putchar
+* main - entry point
 *
-*
+* Return: Always 0 (Success)
 */
-int _putchar(char c);
+int main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -15,6 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 
-_putchar('\n');
- return(0);
+return (0);
 }
