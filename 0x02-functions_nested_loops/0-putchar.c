4ea8ac8b41dcdfab16049d01024b1441cc1f4dac
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-13 14:40:46 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
-echo "Best School" > '\\\'"Best School"\'\\$\?\\\\\:)'
+#include "main.h"
+/**
+* _putchar
+*
+*
+*/
+int _putchar(char c);
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
+_putchar('\n');
+ return(0);
+}
