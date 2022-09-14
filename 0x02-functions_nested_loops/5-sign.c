d1585774956104a03c486f8c888dce6233a8facf
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-13 19:22:14 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#clude "main.h"
+/**
+ * print_sign - Entry point
+ *@n: parameter
+ *Return: Always 0 (success)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+else
+{
+_putchar(45);
+return (-1);
+}
+}
