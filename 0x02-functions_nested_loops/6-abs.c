e028e5e5b7a57d178584481243765251d309f04b
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-15 00:07:12 -0700

    hello

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,12 +4,12 @@
  *@c: is int that will use for the argument of the function
  *Return: 0
  */
-int _abs(int c)
+int '_abs'(int c)
 {
-  if (c >0 || c --0)
-    {
-      return (c);
-    }
-  else
-    return (c * -1);
+if (c > 0 || c -- 0)
+{
+return (c);
+}
+else
+return (c * -1);
 }
