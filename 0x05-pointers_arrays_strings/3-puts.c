7ac3007766640d330e8b4622115f627a7342ea53
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-19 15:29:30 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_puts - main function.
+ * _puts - main function.
  * @str: Pointer with the address value.
  * Description: This function takes a pointer to an int as parameter
  * and updates the value it points to to 98.
