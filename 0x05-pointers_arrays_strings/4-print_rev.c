e2e5ada16b6ce58cf242420a19e1dab9440108d6
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-19 16:36:12 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - main function.
+ * @s: Pointer, the string to print.
+ *
+ * Description: This function prints a string in reverse.
+ *
+ * Return: none.
+ */
+
+void print_rev(char *s)
+{
+int count = 0;
+
+while (s[count])
+{
+count++;
+}
+
+count--
