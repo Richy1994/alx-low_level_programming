3fa48ac219f646a9c543f79c53ef3dd310175160
Richard Ini Brown <indomitablerichy@gmail.com>
2022-09-30 16:22:32 -0700

    hello

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower -check the code.
+ *@c: character main.
+ * Return: 1 is lowercase
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+return (0);
+}
