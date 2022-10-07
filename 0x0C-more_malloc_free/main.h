7fc95727b62803a91407757ba17264336bfa6dee
Richard Ini Brown <indomitablerichy@gmail.com>
2022-10-07 14:10:13 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,11 @@
+fndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
-int _putchar(char C);
-void _putchar(char C);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif /*MAIN_H*/
