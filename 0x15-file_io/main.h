58f6efaf645fd2d6b8c55688540f7106c2126f94
Richard Ini Brown <indomitablerichy@gmail.com>
2022-11-01 05:45:35 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,21 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+fndef _MAIN_H_
+#define _MAIN_H_
 
-/************
- * LIBRARIES
- *************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
 
-/*****************
- * Prototypes*
- *****************/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN.H */
+#endif /* _MAIN_H_ */
